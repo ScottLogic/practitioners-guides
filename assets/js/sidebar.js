db2ea6fc64e8f764c57c6ee26c5b7aea613fdb07
Dean Hunter <dhunter@scottlogic.com>
2024-01-30 09:20:55 +0000

    Dont auto scroll on single column display (mobile)

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const sidebar = document.getElementById("sidebar");
     const sidebarToggle = document.getElementById("sidebar-toggle");
+    const singleColumnWidth = 764;
   
     sidebarToggle.addEventListener("click", function () {
       sidebar.classList.toggle("collapsed");
@@ -31,8 +32,8 @@ document.addEventListener("DOMContentLoaded", function () {
     });
     
     function checkVisible(elm, offset) {
-      var rect = elm.getBoundingClientRect();
-      var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
+      const rect = elm.getBoundingClientRect();
+      const viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
       return !(rect.bottom < 0 || rect.top - viewHeight / offset >= 0);
     }
 
@@ -43,8 +44,10 @@ document.addEventListener("DOMContentLoaded", function () {
             if (navTocLink.innerHTML == visibleSectionTitle.innerHTML) {
               navTocLink.closest("li").classList.add("active-toc");
               // sync scrolling on toc with page
-              if (!checkVisible(navTocLink, 1)) {
-                var navTocLinkBoundingBox = navTocLink.closest("li").getBoundingClientRect();
+              const viewWidth = Math.max(document.documentElement.clientWidth, window.innerWidth);
+              console.log(viewWidth);
+              if (viewWidth > singleColumnWidth && !checkVisible(navTocLink, 1)) {
+                const navTocLinkBoundingBox = navTocLink.closest("li").getBoundingClientRect();
                 sidebar.scrollTo({
                   top: navTocLinkBoundingBox.top,
                   left: 0,
